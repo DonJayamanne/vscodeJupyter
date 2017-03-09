4096a7495f879362acf7334ff753f8be7771ce6a
Don Jayamanne <don.jayamanne@yahoo.com>
2017-03-10 00:45:12 +1100

    fixed to use new techniques

--- webpack.config.js
+++ webpack.config.js
@@ -53,8 +53,8 @@ module.exports = {
       {
         test: /\.css$/,
         loader: ExtractTextPlugin.extract({
-          fallbackLoader: 'style-loader',
-          loader: [{
+          fallback: 'style-loader',
+          use: [{
               loader: 'css-loader',
               query: {
                 modules: true,
