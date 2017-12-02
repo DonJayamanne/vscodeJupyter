51c4d10d61885f6998823a8f5a65f64ef1f11b64
Yu <583181285@qq.com>
2017-12-02 13:44:45 +0800

    💄 remove margin/pading of body

--- src/display/resultView.ts
+++ src/display/resultView.ts
@@ -34,39 +34,37 @@ export class TextDocumentContentProvider extends Disposable implements vscode.Te
         // thinks that there is nothing to be updated.
         let timeNow = new Date().getTime();
         const htmlContent = `
-                    <!DOCTYPE html>
-                    <head>
-                        <style>
-                            html, body {
-                                height: 100%;
-                                width: 100%;
-                            }
-                        </style>
-                        <script>
-                            function start() {
-                                console.log('reloaded results window at time ${timeNow}ms');
-                                var color = '';
-                                var fontFamily = '';
-                                var fontSize = '';
-                                var theme = '';
-                                var fontWeight = '';
-                                try {
-                                    computedStyle = window.getComputedStyle(document.body);
-                                    color = computedStyle.color + '';
-                                    backgroundColor = computedStyle.backgroundColor + '';
-                                    fontFamily = computedStyle.fontFamily;
-                                    fontSize = computedStyle.fontSize;
-                                    fontWeight = computedStyle.fontWeight;
-                                    theme = document.body.className;
-                                } catch(ex) { }
-                                document.getElementById('myframe').src = 'http://localhost:${this.serverPort}/?theme=' + theme + '&color=' + encodeURIComponent(color) + "&backgroundColor=" + encodeURIComponent(backgroundColor) + "&fontFamily=" + encodeURIComponent(fontFamily) + "&fontWeight=" + encodeURIComponent(fontWeight) + "&fontSize=" + encodeURIComponent(fontSize);
-                            }
-                        </script>
-                    </head>
-                    <body onload="start()">
-                        <iframe id="myframe" frameborder="0" style="border: 0px solid transparent; height:100%; width:100%;" src="" seamless></iframe>
-                    </body>
-                    </html>`;
+            <!DOCTYPE html>
+            <head>
+                <style>
+                    html, body { height: 100%; width: 100%; }
+                    body { margin: 0; padding: 0; }
+                </style>
+                <script>
+                    function start() {
+                        console.log('reloaded results window at time ${timeNow}ms');
+                        var color = '';
+                        var fontFamily = '';
+                        var fontSize = '';
+                        var theme = '';
+                        var fontWeight = '';
+                        try {
+                            computedStyle = window.getComputedStyle(document.body);
+                            color = computedStyle.color + '';
+                            backgroundColor = computedStyle.backgroundColor + '';
+                            fontFamily = computedStyle.fontFamily;
+                            fontSize = computedStyle.fontSize;
+                            fontWeight = computedStyle.fontWeight;
+                            theme = document.body.className;
+                        } catch(ex) { }
+                        document.getElementById('myframe').src = 'http://localhost:${this.serverPort}/?theme=' + theme + '&color=' + encodeURIComponent(color) + "&backgroundColor=" + encodeURIComponent(backgroundColor) + "&fontFamily=" + encodeURIComponent(fontFamily) + "&fontWeight=" + encodeURIComponent(fontWeight) + "&fontSize=" + encodeURIComponent(fontSize);
+                    }
+                </script>
+            </head>
+            <body onload="start()">
+                <iframe id="myframe" frameborder="0" style="border: 0px solid transparent; height:100%; width:100%;" src="" seamless></iframe>
+            </body>
+            </html>`;
         return Promise.resolve(htmlContent);
     }
 }
