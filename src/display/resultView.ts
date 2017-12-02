243c90f2658cba6a6af192f258eb6258e07de7ed
Yu <583181285@qq.com>
2017-12-02 13:43:08 +0800

    🎨 improve format

--- src/display/resultView.ts
+++ src/display/resultView.ts
@@ -35,32 +35,38 @@ export class TextDocumentContentProvider extends Disposable implements vscode.Te
         let timeNow = new Date().getTime();
         const htmlContent = `
                     <!DOCTYPE html>
-                    <head><style type="text/css"> html, body{ height:100%; width:100%; } </style>
-                    <script type="text/javascript">
-                        function start(){
-                            console.log('reloaded results window at time ${timeNow}ms');
-                            var color = '';
-                            var fontFamily = '';
-                            var fontSize = '';
-                            var theme = '';
-                            var fontWeight = '';
-                            try {
-                                computedStyle = window.getComputedStyle(document.body);
-                                color = computedStyle.color + '';
-                                backgroundColor = computedStyle.backgroundColor + '';
-                                fontFamily = computedStyle.fontFamily;
-                                fontSize = computedStyle.fontSize;
-                                fontWeight = computedStyle.fontWeight;
-                                theme = document.body.className;
+                    <head>
+                        <style>
+                            html, body {
+                                height: 100%;
+                                width: 100%;
                             }
-                            catch(ex){
+                        </style>
+                        <script>
+                            function start() {
+                                console.log('reloaded results window at time ${timeNow}ms');
+                                var color = '';
+                                var fontFamily = '';
+                                var fontSize = '';
+                                var theme = '';
+                                var fontWeight = '';
+                                try {
+                                    computedStyle = window.getComputedStyle(document.body);
+                                    color = computedStyle.color + '';
+                                    backgroundColor = computedStyle.backgroundColor + '';
+                                    fontFamily = computedStyle.fontFamily;
+                                    fontSize = computedStyle.fontSize;
+                                    fontWeight = computedStyle.fontWeight;
+                                    theme = document.body.className;
+                                } catch(ex) { }
+                                document.getElementById('myframe').src = 'http://localhost:${this.serverPort}/?theme=' + theme + '&color=' + encodeURIComponent(color) + "&backgroundColor=" + encodeURIComponent(backgroundColor) + "&fontFamily=" + encodeURIComponent(fontFamily) + "&fontWeight=" + encodeURIComponent(fontWeight) + "&fontSize=" + encodeURIComponent(fontSize);
                             }
-                            document.getElementById('myframe').src = 'http://localhost:${this.serverPort}/?theme=' + theme + '&color=' + encodeURIComponent(color) + "&backgroundColor=" + encodeURIComponent(backgroundColor) + "&fontFamily=" + encodeURIComponent(fontFamily) + "&fontWeight=" + encodeURIComponent(fontWeight) + "&fontSize=" + encodeURIComponent(fontSize);
-                        }
-                    </script>
+                        </script>
                     </head>
                     <body onload="start()">
-                    <iframe id="myframe" frameborder="0" style="border: 0px solid transparent;height:100%;width:100%;" src="" seamless></iframe></body></html>`;
+                        <iframe id="myframe" frameborder="0" style="border: 0px solid transparent; height:100%; width:100%;" src="" seamless></iframe>
+                    </body>
+                    </html>`;
         return Promise.resolve(htmlContent);
     }
 }
